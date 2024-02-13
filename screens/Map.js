54cb233ea6d270e86e511b0eca0bf83b15537b88
Santeri <santeri.hyry@gmail.com>
2024-02-13 14:34:59 +0200

    exercise 1

---
+++ screens/Map.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import MapView from 'react-native-maps';
+
+export default function Map() {
+    return (
+        <MapView style={styles.map} />
+    );
+}
+
+const styles = StyleSheet.create({
+    map: {
+        height: "100%",
+        width: "100%",
+    }
+});
