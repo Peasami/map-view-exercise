3b0f2795994edd399af8133b7d2411a4a5174c55
Santeri <santeri.hyry@gmail.com>
2024-02-13 14:44:33 +0200

    exercise 2

--- screens/Map.js
+++ screens/Map.js
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function Map() {
+
+    const [location, setLocation] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+
+    const getUserPosition = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        try {
+            if (status !== "granted") {
+                console.log("Permission to access location was denied");
+                return;
+            }
+            const position = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
+            setLocation({ ...location, "latitude": position.coords.latitude, "longitude": position.coords.longitude });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        (async() => {
+            getUserPosition();
+        })();
+    }, []);
+
     return (
-        <MapView style={styles.map} />
+        <MapView
+            style={styles.map}
+            region={location}
+        />
     );
 }
+
+
 
 const styles = StyleSheet.create({
     map: {
