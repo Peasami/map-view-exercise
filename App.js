eea40cdc28db283d366d5213753bc4fcebd14e82
Santeri <santeri.hyry@gmail.com>
2024-02-13 14:37:00 +0200

    exercise 1 bugfix

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import Map from './screens/Map';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
   },
 });
